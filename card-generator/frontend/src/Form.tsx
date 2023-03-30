dea12aa98d3215665bfbc298f8fc7004fa9c7752
Kurogoma4D <highdro0479@gmail.com>
2023-03-30 22:39:49 +0900

    add: vertical mode

--- card-generator/frontend/src/Form.tsx
+++ card-generator/frontend/src/Form.tsx
@@ -1,10 +1,12 @@
-import { FC } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { FieldErrors, useForm, UseFormRegister } from 'react-hook-form';
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import { FieldErrors, useForm, UseFormRegister } from "react-hook-form";
+import { Member } from "./member";
 
 interface FormType {
   left: Member[];
   right: Member[];
+  vertical: boolean;
 }
 
 interface InputProps {
@@ -13,14 +15,14 @@ interface InputProps {
   errors: FieldErrors<FormType>;
 }
 
-const inputClassName = 'p-3 border border-neutral-200 rounded-lg';
+const inputClassName = "p-3 border border-neutral-200 rounded-lg";
 const buttonClassNamePrimary =
-  'px-3 py-2 rounded-lg bg-neutral-700 text-white font-bold w-max cursor-pointer';
+  "px-3 py-2 rounded-lg bg-neutral-700 text-white font-bold w-max cursor-pointer";
 const buttonClassNameSecondary =
-  'px-3 py-2 border-2 border-neutral-200 rounded-lg text-neutral-500 font-bold w-max cursor-pointer';
-const errorMessageClassName = 'text-red-900';
+  "px-3 py-2 border-2 border-neutral-200 rounded-lg text-neutral-500 font-bold w-max cursor-pointer";
+const errorMessageClassName = "text-red-900";
 
-const requiredMessage = '入力必須です';
+const requiredMessage = "入力必須です";
 
 const Form: FC = () => {
   const {
@@ -33,12 +35,14 @@ const Form: FC = () => {
   const onSubmit = (data: FormType) => {
     const left = btoa(JSON.stringify([...data.left]));
     const right = btoa(JSON.stringify([...data.right]));
+    const vertical = data.vertical;
 
     const builtParams = new URLSearchParams({
       left: left,
       right: right,
+      vertical: `${vertical}`,
     }).toString();
-    window.open('/multiple?' + builtParams, '_blank');
+    window.open("/multiple?" + builtParams, "_blank");
   };
 
   return (
@@ -50,6 +54,19 @@ const Form: FC = () => {
       <div className="p-5 bg-blue-100 flex flex-col gap-3">
         <RightInputMember index={0} register={register} errors={errors} />
         <RightInputMember index={1} register={register} errors={errors} />
+      </div>
+      <div>Vertical:</div>
+      <div>
+        <label>
+          <input type="radio" value="true" {...register("vertical")} />
+          true
+        </label>
+      </div>
+      <div>
+        <label>
+          <input type="radio" value="false" {...register("vertical")} />
+          false
+        </label>
       </div>
 
       <div className="m-4 flex gap-4">
@@ -98,6 +115,11 @@ const LeftInputMember: FC<InputProps> = ({ index, register, errors }) => {
       {errors.left && (
         <p className={errorMessageClassName}>{errors.left.message}</p>
       )}
+      <SelectVariant
+        side="left"
+        index={index}
+        register={register}
+      ></SelectVariant>
     </>
   );
 };
@@ -130,6 +152,56 @@ const RightInputMember: FC<InputProps> = ({ index, register, errors }) => {
       {errors.right && (
         <p className={errorMessageClassName}>{errors.right.message}</p>
       )}
+      <SelectVariant
+        side="right"
+        index={index}
+        register={register}
+      ></SelectVariant>
+    </>
+  );
+};
+
+interface VariantProps {
+  side: "left" | "right";
+  index: number;
+  register: UseFormRegister<FormType>;
+}
+
+const SelectVariant: FC<VariantProps> = ({ side, index, register }) => {
+  return (
+    <>
+      <label>
+        <input
+          type="radio"
+          value="red"
+          {...register(`${side}.${index}.variant`)}
+        ></input>
+        red
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="blue"
+          {...register(`${side}.${index}.variant`)}
+        ></input>
+        blue
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="green"
+          {...register(`${side}.${index}.variant`)}
+        ></input>
+        green
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="yellow"
+          {...register(`${side}.${index}.variant`)}
+        ></input>
+        yellow
+      </label>
     </>
   );
 };
--- card-generator/frontend/src/MultipleCardView.tsx
+++ card-generator/frontend/src/MultipleCardView.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Member } from "./member";
 
 const MultipleCardView: FC = () => {
   const [searchParams] = useSearchParams();
 
   const leftParams = searchParams.get("left");
   const rightParams = searchParams.get("right");
+  const isVertical = searchParams.get("vertical") ?? false;
   if (!leftParams || !rightParams) {
     return <></>;
   }
@@ -18,13 +20,34 @@ const MultipleCardView: FC = () => {
   const leftMembers = extractMembers(leftParams);
   const rightMembers = extractMembers(rightParams);
 
+  if (isVertical) {
+    return (
+      <>
+        <div className="bg-black">
+          {leftMembers.map((member) => (
+            <div key={member.name}>{_CardViewForVertical({ ...member })}</div>
+          ))}
+          {rightMembers.map((member) => (
+            <div key={member.name}>{_CardViewForVertical({ ...member })}</div>
+          ))}
+        </div>
+        {/* <img
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/5 z-50"
+          src="vs.png"
+        /> */}
+      </>
+    );
+  }
+
   return (
     <>
       <div className="flex justify-center items-center bg-black">
-        {leftMembers.map((member) => _CardView({ ...member, variant: "red" }))}
-        {rightMembers.map((member) =>
-          _CardView({ ...member, variant: "blue" })
-        )}
+        {leftMembers.map((member) => (
+          <div key={member.name}>{_CardView({ ...member })}</div>
+        ))}
+        {rightMembers.map((member) => (
+          <div key={member.name}>{_CardView({ ...member })}</div>
+        ))}
       </div>
       <img
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/5 z-50"
@@ -49,28 +72,50 @@ const cardViewVariants = {
   },
 };
 
-interface _CardViewProps extends Member {
-  variant: keyof typeof cardViewVariants;
-}
-
-const _CardView: FC<_CardViewProps> = ({
+const _CardView: FC<Member> = ({
   name,
   url,
   variant,
 }: {
   name: string;
   url: string;
-  variant: keyof typeof cardViewVariants;
+  variant: keyof typeof cardViewVariants | null;
 }) => {
   return (
     <div className="relative w-1/2 h-screen flex justify-center">
       <img src={url} alt="Left Image" />
       <p
-        className={`absolute top-5 left-0 w-full z-5 p-5 text-white text-center -skew-y-6 text-5xl drop-shadow-2xl ${cardViewVariants[variant].bg}`}
+        className={`absolute top-5 left-0 w-full z-5 p-5 text-white text-center -skew-y-6 text-5xl drop-shadow-2xl ${
+          cardViewVariants[variant ?? "red"].bg
+        }`}
       >
         {name}
       </p>
     </div>
   );
 };
+
+const _CardViewForVertical: FC<Member> = ({
+  name,
+  url,
+  variant,
+}: {
+  name: string;
+  url: string;
+  variant: keyof typeof cardViewVariants | null;
+}) => {
+  return (
+    <div className="relative block w-full">
+      <img src={url} alt="Image" />
+      <p
+        className={`absolute top-5 left-0 w-full z-5 p-5 text-white text-center -skew-y-6 text-5xl drop-shadow-2xl ${
+          cardViewVariants[variant ?? "red"].bg
+        }`}
+      >
+        {name}
+      </p>
+    </div>
+  );
+};
+
 export default MultipleCardView;
